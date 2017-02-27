80bde3f4c3c9fcac25c925b5b89fe18fd831afe5
Troy Scott <troys2005@gmail.com>
2017-02-26 16:27:02 -0800

    intitial checkin

---
+++ BoxModelDemo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+const BoxModelDemo = () => (
+    <View>
+        <Text>Column 1</Text>
+    </View>
+);
+
+module.exports = BoxModelDemo;
